468a59703512c4349f5c387f90d212f34141eed7
jingxiongdi <937969716@qq.com>
2018-08-22 19:53:01 +0800

    上传基础工程

---
+++ app/src/main/java/com/jxd/myproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jxd.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
